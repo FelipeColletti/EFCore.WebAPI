8e6adf0c4bd2dc0ebface6b09e4c965dc7570c0f
FelipeColletti <Colletti.felipe@gmail.com>
2020-05-02 16:52:56 -0300

    Inserindo camadas e mostrando o CRUD basico

--- EFCore.Dominio/Batalha.cs
+++ EFCore.Dominio/Batalha.cs
@@ -1,14 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace EFCore.WebAPI.Models
+namespace EFCore.Dominio
 {
     public class Batalha
     {
         public int Id { get; set; }
-        public string Nome { get; set; }   
+        public string Nome { get; set; }
         public string Descricao { get; set; }
         public DateTime DtInicio { get; set; }
         public DateTime DtFim { get; set; }
---
+++ EFCore.Repo/HeroiContexto.cs
@@ -0,0 +1,25 @@
+﻿using EFCore.Dominio;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.Repo
+{
+    public class HeroiContexto : DbContext
+    {
+ 
+        public HeroiContexto(DbContextOptions<HeroiContexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadeSecretas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity => { entity.HasKey(e => new { e.BatalhaId, e.HeroiID }); });
+        }
+    }
+}
